8e403dff45ad8e4bf132de34758c46d882d75c81
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-06-20 13:43:01 +0530

    preparing data for training model

--- src/Data_preparation.py
+++ src/Data_preparation.py
@@ -1,11 +1,13 @@
+import json
+
 class Review:
-    def __init__(self,review_text,rate):
+    def __init__(self, review_text, rate):
         self.review_text = review_text
         self.rate = rate
         self.sentiment = self.get_sentiment(rate)
 
-    def get_sentiment(self,rate):
-        if rate<=2:
+    def get_sentiment(self, rate):
+        if rate <= 2:
             return 'NEGATIVE'
         elif rate == 3:
             return 'NEUTRAL'
@@ -13,8 +15,9 @@ class Review:
             return 'POSITIVE'
 
 
-
-with open('data_reviews') as f:
-    file = f.readlines()
-    for i in file:
-
+with open('Books_small.json') as f:
+    for line in f:
+        l = json.loads(line)
+        review = l['reviewText']
+        print(review)
+        break
