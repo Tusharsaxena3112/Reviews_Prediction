b64fb0d0bbed96dcad261025b3c2213c46e91579
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-06-20 14:58:08 +0530

    adding bags of words representation using sklearn CountVectorizer

--- src/Data_preparation.py
+++ src/Data_preparation.py
@@ -1,5 +1,8 @@
 import json
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
 
 class Review:
     def __init__(self, review_text, rate):
@@ -21,3 +24,16 @@ with open('Books_small.json') as f:
     for line in f:
         l = json.loads(line)
         reviews.append(Review(l['reviewText'], l['overall']))
+
+train_reviews, test_reviews = train_test_split(reviews, test_size=0.3, random_state=42)
+
+train_reviews_text = [x.review_text for x in train_reviews]
+train_review_sentiments = [x.sentiment for x in train_reviews]
+
+test_reviews_text = [x.review_text for x in test_reviews]
+test_reviews_sentiments = [x.sentiment for x in test_reviews]
+
+vectorizer = CountVectorizer()
+
+test_reviews_text_final = vectorizer.fit_transform(
+    train_reviews_text)  # bags of words (to convert text data into # numerical data)
