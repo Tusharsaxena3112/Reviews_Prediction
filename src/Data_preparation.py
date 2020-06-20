e0f8b9aaebd16b9fdeba77ea54bb835395cc10eb
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-06-20 13:14:43 +0530

    preparing data

--- src/Data_preparation.py
+++ src/Data_preparation.py
@@ -2,10 +2,17 @@ class Review:
     def __init__(self,review_text,rate):
         self.review_text = review_text
         self.rate = rate
-        self.sentiment = self.get_sentiment()
+        self.sentiment = self.get_sentiment(rate)
 
-    def get_sentiment(self):
-        
+    def get_sentiment(self,rate):
+        if rate<=2:
+            return 'NEGATIVE'
+        elif rate == 3:
+            return 'NEUTRAL'
+        else:
+            return 'POSITIVE'
+
+
 
 with open('data_reviews') as f:
     file = f.readlines()
