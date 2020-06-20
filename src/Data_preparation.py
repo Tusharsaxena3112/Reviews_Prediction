19475826c05c1daf15d68e67da300a2acc40827e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-06-20 19:48:12 +0530

    evaluating models using sklearn f1_score() function

--- src/Data_preparation.py
+++ src/Data_preparation.py
@@ -32,25 +32,39 @@ with open('Books_small.json') as f:
 reviews_text = [x.review_text for x in reviews]
 review_sentiments = [x.sentiment for x in reviews]
 
-vectorizer = CountVectorizer()
-
-reviews_text_final = vectorizer.fit_transform(
-    reviews_text)  # bags of words (to convert text data into # numerical data)
-
 # splitting data into train and test
 train_reviews_text, test_reviews_text, train_reviews_sentiments, test_reviews_sentiments = train_test_split(
-    reviews_text_final, review_sentiments, test_size=0.3, random_state=42)
+    reviews_text, review_sentiments, test_size=0.3, random_state=42)
+
+vectorizer = CountVectorizer()
+
+train_reviews_text_vector = vectorizer.fit_transform(
+    train_reviews_text)  # bags of words (to convert text data into # numerical data)
+
+test_reviews_text_vector = vectorizer.transform(test_reviews_text)
 
 # first model ie K -nn from sklearn
 
 knn = KNeighborsClassifier(n_neighbors=6)
-knn.fit(train_reviews_text, train_reviews_sentiments)
-predictions_knn = knn.predict(test_reviews_text)
+knn.fit(train_reviews_text_vector, train_reviews_sentiments)
+predictions_knn = knn.predict(test_reviews_text_vector)
 accuracy_score_knn = accuracy_score(np.array(test_reviews_sentiments), predictions_knn)
 
 # Another model ie Decision Tree
 
 dec = DecisionTreeClassifier(random_state=42)
-dec.fit(train_reviews_text, train_reviews_sentiments)
-predication_dec = dec.predict(test_reviews_text)
-accuracy_score_dec = accuracy_score(test_reviews_sentiments, predication_dec)
+dec.fit(train_reviews_text_vector, train_reviews_sentiments)
+predication_dec = dec.predict(test_reviews_text_vector)
+accuracy_score_dec = accuracy_score(np.array(test_reviews_sentiments), predication_dec)
+
+# Evaluation of the models
+
+from sklearn.metrics import f1_score
+
+f1_score_knn = f1_score(test_reviews_sentiments, predictions_knn, average=None,
+                        labels=['POSITIVE', 'NEGATIVE', 'NEUTRAL'])
+print(f1_score_knn)
+
+f1_score_dec = f1_score(test_reviews_sentiments, predication_dec, labels=['POSITIVE', 'NEGATIVE', 'NEUTRAL'],
+                        average=None)
+print(f1_score_dec)
